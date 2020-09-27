64f5bf986cd0e8cb14acd75ebaf03b49c47b6a33
kzz <jackouzz@hotmail.com>
2020-09-27 18:43:32 +0800

    game loop control

--- 07_game_loop.py
+++ 07_game_loop.py
@@ -6,13 +6,17 @@ screen = pygame.display.set_mode((480, 700))
 
 bg = pygame.image.load("./images/background.png")
 screen.blit(bg, (0, 0))
-pygame.display.update()
-
 hero = pygame.image.load("./images/me2.png")
 screen.blit(hero, (150, 500))
 pygame.display.update()
 
+i = 0
+
+clock = pygame.time.Clock()
+
 while True:
-    pass
+    clock.tick(1)
+    print(i)
+    i += 1
 
 pygame.quit()
