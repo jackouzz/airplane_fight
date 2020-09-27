9ab60404e25323bec45e26bbcf78915ced7f9bd5
kzz <jackouzz@hotmail.com>
2020-09-27 17:19:16 +0800

    draw hero's aircraft

---
+++ 05_draw_hero.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((480, 700))
+
+bg = pygame.image.load("./images/background.png")
+
+screen.blit(bg, (0, 0))
+
+pygame.display.update()
+
+hero = pygame.image.load("./images/me2.png")
+screen.blit(hero, (150, 500))
+pygame.display.update()
+
+while True:
+    pass
+
+pygame.quit()
