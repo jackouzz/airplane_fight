c2cfb488d6f1910b3485b05698605ee80535b0cc
kzz <jackouzz@hotmail.com>
2020-09-29 17:08:55 +0800

    make game structure II,start game method

--- plane_main.py
+++ plane_main.py
@@ -20,7 +20,35 @@ class PlaneGame(object):
     def start_game(self):
         print("start game...")
         while True:
+            # 1. set frequency
+            self.clock.tick(FRAME_PER_SEC)
+            # 2. event listening
+            self.__event_handler()
+            # 3. detective crash
+            self.__check_collide()
+            # 4. update and draw sprites and their group
+            self.__update_sprites()
+            # 5. update display
+            pygame.display.update()
+
             pass
+
+    def __event_handler(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                PlaneGame.__game_over()
+
+    def __check_collide(self):
+        pass
+
+    def __update_sprites(self):
+        pass
+
+    @staticmethod
+    def __game_over():
+        print("GAME OVER")
+        pygame.quit()
+        exit()
 
 
 if __name__ == '__main__':
--- plane_sprites.py
+++ plane_sprites.py
@@ -3,6 +3,11 @@ import pygame
 # screen size
 SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
 
+
+# frequency
+FRAME_PER_SEC = 60
+
+
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image_name, speed=1):
         super().__init__()
