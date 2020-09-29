cd58a32c4ccff7a9835a458b4143193f94375671
kzz <jackouzz@hotmail.com>
2020-09-29 11:40:52 +0800

    make game structure

---
+++ plane_main.py
@@ -0,0 +1,13 @@
+class PlaneGame(object):
+    """plane game main program"""
+
+    def __init__(self):
+        print("game init...")
+
+    def start_game(self):
+        print("satrt game...")
+
+
+if __name__ == '__main__':
+    game = PlaneGame()
+    game.start_game()
