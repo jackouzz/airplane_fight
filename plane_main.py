4d0604fb41cbeddf8be543a48bdfe18e070c1867
kzz <jackouzz@hotmail.com>
2020-09-29 12:09:06 +0800

    make game structure II

--- plane_main.py
+++ plane_main.py
@@ -1,11 +1,24 @@
+import pygame
+from plane_sprites import *
+
+
 class PlaneGame(object):
     """plane game main program"""
 
     def __init__(self):
         print("game init...")
+        # 1. create game's windows
+        self.screen = pygame.display.set_mode((480, 700))
+        # 2. create game's clock
+        self.clock = pygame.time.Clock()
+        # 3. call private methods ,create sprites and their group
+        self.__create_sprites()
+
+    def __create_sprites(self):
+        pass
 
     def start_game(self):
-        print("satrt game...")
+        print("start game...")
 
 
 if __name__ == '__main__':
