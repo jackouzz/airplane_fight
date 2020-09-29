c36584ee8fef4ab9575980331ead5d21b82b3c51
kzz <jackouzz@hotmail.com>
2020-09-29 12:38:51 +0800

    make game structure II,use constant

--- plane_main.py
+++ plane_main.py
@@ -8,7 +8,7 @@ class PlaneGame(object):
     def __init__(self):
         print("game init...")
         # 1. create game's windows
-        self.screen = pygame.display.set_mode((480, 700))
+        self.screen = pygame.display.set_mode(SCREEN_RECT.size)
         # 2. create game's clock
         self.clock = pygame.time.Clock()
         # 3. call private methods ,create sprites and their group
@@ -19,6 +19,8 @@ class PlaneGame(object):
 
     def start_game(self):
         print("start game...")
+        while True:
+            pass
 
 
 if __name__ == '__main__':
